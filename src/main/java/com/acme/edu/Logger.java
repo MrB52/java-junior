c042f6a57a3cec28b7529015135631d69426c5ee
MrB52 <bondar2504@mail.ru>
2018-02-28 11:45:19 +0300

    Course Project Iteration 1: almost DONE

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,11 +1,48 @@
 package com.acme.edu;
 
 public class Logger {
+
+    public static String addPrefix(int choice){
+        switch (choice) {
+            case 1:
+                return "primitive: ";
+            case 2:
+                return "char: ";
+            case 3:
+                return "string: ";
+            case 4:
+                return "reference: ";
+            default:
+                return "";
+        }
+    }
+
+//    public static void print(int choice, int message) {
+//        System.out.println(addPrefix(1) + message);
+//    }
+
+    //TODO подумать над повторным использованием кода
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        System.out.println(addPrefix(1) + message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        System.out.println(addPrefix(1) + message);
+    }
+
+    public static void log(char message) {
+        System.out.println(addPrefix(2) + message);
+    }
+
+    public static void log(String message) {
+        System.out.println(addPrefix(3) + message);
+    }
+
+    public static void log(boolean message) {
+      System.out.println(addPrefix(1) + message);
+    }
+
+    public static void log(Object message) {
+        System.out.println(addPrefix(4) + message);
     }
 }
