f01d7efd0ded78c58d04de4fcd9279c761489b88
Aleksey <bondar2504@mail.ru>
2018-03-11 21:13:24 +0300

    Course Project. Code clean up

--- src/main/java/com/acme/edu/printer/Printer.java
+++ src/main/java/com/acme/edu/printer/Printer.java
@@ -1,8 +1,6 @@
 package com.acme.edu.printer;
 
-import com.acme.edu.message.LogMessage;
 
 public interface Printer {
-//    void printOut(LogMessage message);
     void printOut(String message);
 }
