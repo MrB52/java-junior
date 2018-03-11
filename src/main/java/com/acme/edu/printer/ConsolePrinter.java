3b8bf41c21e7b582586c8c6515391a0c5ab1da6c
Aleksey <bondar2504@mail.ru>
2018-03-11 21:12:16 +0300

    Course Project. Code clean up

--- src/main/java/com/acme/edu/printer/ConsolePrinter.java
+++ src/main/java/com/acme/edu/printer/ConsolePrinter.java
@@ -1,12 +1,6 @@
 package com.acme.edu.printer;
 
-import com.acme.edu.message.LogMessage;
-
 public class ConsolePrinter implements Printer{
-//    @Override
-//    public void printOut(LogMessage message) {
-//        System.out.println(message);
-//    }
 
     @Override
     public void printOut(String message) {
